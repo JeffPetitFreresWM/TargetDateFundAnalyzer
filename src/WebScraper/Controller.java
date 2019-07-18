6d2197ced22534e3dfe35f98f819d16ae7909811
JeffPetitfreresWM <jwpetitfreres@email.wm.edu>
2019-07-18 17:01:07 -0400

    Updated Ui

--- src/WebScraper/Controller.java
+++ src/WebScraper/Controller.java
@@ -1,18 +1,41 @@
 package WebScraper;
 
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
-import java.io.FileInputStream;
-import java.io.IOException;
 
 public class Controller {
 
-}
+    @FXML
+    private ComboBox<?> dropdown;
 
+    @FXML
+    private Button analyzeButton;
+
+    @FXML
+    private LineChart<?, ?> lineGraph;
+
+    @FXML
+    private VBox info2020;
+
+    @FXML
+    private VBox info2030;
+
+    @FXML
+    private VBox info2040;
+
+    @FXML
+    private VBox info2050;
+
+    @FXML
+    private VBox info2060;
+
+    @FXML
+    void analyze(ActionEvent event) {
+
+    }
+
+}
